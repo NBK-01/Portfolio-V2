143c5edc4fd7b975c403015b23e5f91e47779899
unknown <nayefkanaan03@gmail.com>
2021-08-18 03:43:23 +0300

    WIP

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".copy", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".anim", { y: "-100%", duration: 1.5, delay: 0.5 });
+
+tl.to(".anim", { y: "-100%", duration: 1.5, delay: 0.2 });
 tl.to(".trans", { y: "-100%", duration: 1}, "-=1");

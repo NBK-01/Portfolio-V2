d644e97ebe4922efecb599a5ed9234155f99c045
unknown <nayefkanaan03@gmail.com>
2021-08-20 18:00:27 +0300

    Added mobile nav bar to first page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 // alert("Hey there! viewing this webpage on desktop is recommended")
 
-let aboutBtn = document.querySelector(".abt-btn")
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+
+
+
+let aboutBtn = document.querySelector(".abt-btn-mobile")
 var aboutPage = 'about.html'
 
 aboutBtn.addEventListener("click", function(){

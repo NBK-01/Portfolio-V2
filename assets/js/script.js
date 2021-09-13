2ae5b62c8a8f2ec62d5ca8893cb60df89df616fc
unknown <nayefkanaan03@gmail.com>
2021-09-13 18:33:16 +0300

    initialized AOS lib

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
 // alert("Hey there! viewing this webpage on desktop is recommended")
 
+AOS.init();
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
 
-
-
-
-
   
-
-
 let contactBtn = document.querySelector(".contact-btn")
 let contactBtnMobile = document.querySelector(".contact-btn-mobile")
 var contactPage = 'contact.html'
